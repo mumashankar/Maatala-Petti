bf1db94915756a3ba5663b794ea00992e911c20b
Uma Shankar <mr.umashankar@gmail.com>
2025-12-29 21:31:32 +0530

    Initial commit: Setup SpringBoot application

---
+++ src/main/java/com/learning/java/uma/MaatalaPetti/MaatalaPettiApplication.java
@@ -0,0 +1,13 @@
+package com.learning.java.uma.MaatalaPetti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaatalaPettiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaatalaPettiApplication.class, args);
+	}
+
+}
